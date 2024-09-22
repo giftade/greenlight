fb91e326b93b5849dc52d8faddd86ee2b7fe6b7a
Segun Ade <giftsegunade@gmail.com>
2024-09-22 14:33:44 +0100

    used readParamID function instead

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -3,23 +3,17 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"strconv"
-
-	"github.com/julienschmidt/httprouter"
 )
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "create new movie")
 }
 
-func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request){
-	params := httprouter.ParamsFromContext(r.Context())
-
-	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
-	if err != nil || id < 1 {
+func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
+	id, err := app.readIDParam(r)
+	if err != nil {
 		http.NotFound(w, r)
 		return
 	}
-
-	fmt.Fprintf(w, "show details of movie with id of %d\n", id)
+	fmt.Fprintf(w, "show the details of movie %d\n", id)
 }
