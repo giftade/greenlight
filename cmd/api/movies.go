da951e2c9aa42e5ac3c7ae480ef3ec09f2e8ba7c
Segun Ade <giftsegunade@gmail.com>
2024-09-25 13:55:23 +0100

    implement validation

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/giftade/greenlight/internal/data"
+	"github.com/giftade/greenlight/internal/validator"
 )
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
@@ -21,6 +22,30 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 		  app.badRequestResponse(w, r, err) 
 		return
 	}
+
+	v := validator.New()
+
+	v.Check(input.Title != "", "title", "must be provided")
+	v.Check(len(input.Title) <= 500, "title", "must not be less than 500 bytes long")
+
+	v.Check(input.Year != 0, "year", "must be provided")
+	v.Check(input.Year >= 1888, "year", "must be greater than 1888")
+	v.Check(input.Year <= int32(time.Now().Year()), "year", "must not be in the future")
+
+	v.Check(input.Runtime != 0, "runtime", "must be provided")
+	v.Check(input.Runtime > 0, "runtime", "must be a positive integer")
+
+	v.Check(input.Genres != nil, "genres", "must be provided")
+	v.Check(len(input.Genres) >= 1, "genres", "must contain at least genre")
+	v.Check(len(input.Genres) <= 5, "genres", "must not contain more than 5 genres")
+
+	v.Check(validator.Unique(input.Genres), "genres", "must not contain duplicate values")
+
+	if !v.Valid(){
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
 	fmt.Fprintf(w, "%+v\n", input)
 }
 
