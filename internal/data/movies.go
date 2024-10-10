884525351bb443fc5810655c3669fbd762899f3d
Segun Ade <giftsegunade@gmail.com>
2024-10-10 11:28:10 +0100

    wrote code for get function

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -2,6 +2,7 @@ package data
 
 import (
 	"database/sql"
+	"errors"
 	"time"
 
 	"github.com/giftade/greenlight/internal/validator"
@@ -42,26 +43,51 @@ type MovieModel struct {
 	DB *sql.DB
 }
 
-
-func(m MovieModel) Insert(movie *Movie) error {
-	  query := `
+func (m MovieModel) Insert(movie *Movie) error {
+	query := `
         INSERT INTO movies (title, year, runtime, genres) 
         VALUES ($1, $2, $3, $4)
         RETURNING id, created_at, version`
 
-		args := []interface{}{&movie.Title, &movie.Year, movie.Runtime, pq.Array(&movie.Genres)}
-		
-		return m.DB.QueryRow(query, args...).Scan(&movie.ID, &movie.CreatedAt, &movie.Version)
+	args := []interface{}{&movie.Title, &movie.Year, movie.Runtime, pq.Array(&movie.Genres)}
+
+	return m.DB.QueryRow(query, args...).Scan(&movie.ID, &movie.CreatedAt, &movie.Version)
 }
 
-func(m MovieModel) Get(id int64) (*Movie, error){
-	return nil, nil
+func (m MovieModel) Get(id int64) (*Movie, error) {
+	if id < 1 {
+		return nil, ErrRecordNotFound
+	}
+	var movie Movie
+	query := `
+		 SELECT id, created_at, title, year, runtime, genres, version
+ 		 FROM movies
+ 		 WHERE id = $1`
+
+	err := m.DB.QueryRow(query, id).Scan(
+		&movie.ID,
+		&movie.CreatedAt,
+		&movie.Title,
+		&movie.Year,
+		&movie.Runtime,
+		pq.Array(&movie.Genres),
+		&movie.Version,
+	)
+	if err != nil {
+		switch{
+		case errors.Is(err, sql.ErrNoRows):
+			return nil, ErrRecordNotFound
+		default:
+			return nil, err
+		}
+	}
+	return &movie, nil
 }
 
-func(m MovieModel) Update(movie *Movie) error{
+func (m MovieModel) Update(movie *Movie) error {
 	return nil
 }
 
-func(m MovieModel) Delete(id int64) error{
+func (m MovieModel) Delete(id int64) error {
 	return nil
 }
