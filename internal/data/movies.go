2d92e5853cedfc5831451336e623cfbecc9eeb2a
Segun Ade <giftsegunade@gmail.com>
2024-10-18 19:18:08 +0100

    add 3 second timeout deadline for database functions

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -1,6 +1,7 @@
 package data
 
 import (
+	"context"
 	"database/sql"
 	"errors"
 	"time"
@@ -50,21 +51,27 @@ func (m MovieModel) Insert(movie *Movie) error {
         RETURNING id, created_at, version`
 
 	args := []interface{}{&movie.Title, &movie.Year, movie.Runtime, pq.Array(&movie.Genres)}
+	
+	ctx, cancel := context.WithTimeout(context.Background(), 3 *time.Second)
+	defer cancel()
 
-	return m.DB.QueryRow(query, args...).Scan(&movie.ID, &movie.CreatedAt, &movie.Version)
+	return m.DB.QueryRowContext(ctx, query, args...).Scan(&movie.ID, &movie.CreatedAt, &movie.Version)
 }
 
 func (m MovieModel) Get(id int64) (*Movie, error) {
 	if id < 1 {
 		return nil, ErrRecordNotFound
 	}
-	var movie Movie
 	query := `
 		 SELECT id, created_at, title, year, runtime, genres, version
  		 FROM movies
- 		 WHERE id = $1`
+		 WHERE id = $1`
+	var movie Movie
 
-	err := m.DB.QueryRow(query, id).Scan(
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	err := m.DB.QueryRowContext(ctx, query, id).Scan(
 		&movie.ID,
 		&movie.CreatedAt,
 		&movie.Title,
@@ -93,7 +100,10 @@ func (m MovieModel) Update(movie *Movie) error {
 
 	args := []interface{}{movie.Title, movie.Year, movie.Runtime, pq.Array(movie.Genres), movie.ID, movie.Version}
 
-	err :=  m.DB.QueryRow(query, args...).Scan(&movie.Version)
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	err := m.DB.QueryRowContext(ctx, query, args...).Scan(&movie.Version)
 	if err != nil {
 		switch {
 		case errors.Is(err, sql.ErrNoRows):
@@ -112,7 +122,9 @@ func (m MovieModel) Delete(id int64) error {
 	query := `
         DELETE FROM movies
         WHERE id = $1`
-	result, err := m.DB.Exec(query, id)
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+	result, err := m.DB.ExecContext(ctx, query, id)
 	if err != nil {
 		return err
 	}
