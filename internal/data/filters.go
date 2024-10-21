c6a3df1dc2b8aa2150f0bfefd7b010f246f14f5d
Segun Ade <giftsegunade@gmail.com>
2024-10-21 11:57:44 +0100

    return metadata as part of response when all movies list is requested

--- internal/data/filters.go
+++ internal/data/filters.go
@@ -1,6 +1,7 @@
 package data
 
 import (
+	"math"
 	"strings"
 
 	"github.com/giftade/greenlight/internal/validator"
@@ -14,7 +15,7 @@ type Filters struct {
 }
 
 func (f Filters) sortColumn() string {
-	for _, safeValue := range f.SortSafelist{
+	for _, safeValue := range f.SortSafelist {
 		if f.Sort == safeValue {
 			return strings.TrimPrefix(f.Sort, "-")
 		}
@@ -23,16 +24,16 @@ func (f Filters) sortColumn() string {
 }
 
 func (f Filters) sortDirection() string {
-	if strings.HasPrefix(f.Sort, "-"){
+	if strings.HasPrefix(f.Sort, "-") {
 		return "DESC"
 	}
 	return "ASC"
 }
 
-func (f Filters) limit() int{
+func (f Filters) limit() int {
 	return f.PageSize
 }
-func (f Filters) offset() int{
+func (f Filters) offset() int {
 	return (f.Page - 1) * f.PageSize
 }
 
@@ -42,4 +43,26 @@ func ValidateFilters(v *validator.Validator, f Filters) {
 	v.Check(f.PageSize > 0, "page_size", "must be greater than zero")
 	v.Check(f.PageSize <= 100, "page_size", "must be a maximum of 100")
 	v.Check(validator.In(f.Sort, f.SortSafelist...), "sort", "invalid sort value")
+}
+
+type MetaData struct {
+	CurrentPage  int `json:"current_page,omitempty"`
+	PageSize     int `json:"page_size,omitempty"`
+	FirstPage    int `json:"first_page,omitempty"`
+	LastPage     int `json:"last_page,omitempty"`
+	TotalRecords int `json:"total_records,omitempty"`
+}
+
+func calculateMetaData(totalRecords, page, pageSize int) MetaData {
+	if totalRecords == 0 {
+		return MetaData{}
+	}
+	return MetaData{
+		CurrentPage:  page,
+		PageSize:     pageSize,
+		FirstPage:    1,
+		LastPage:     int(math.Ceil(float64(totalRecords) / float64(pageSize))),
+		TotalRecords: totalRecords,
+	}
+
 }
