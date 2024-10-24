eb5236e5887ca4674a84c82573ad1be70358c921
Segun Ade <giftsegunade@gmail.com>
2024-10-24 11:04:02 +0100

    graceful shutdown

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -2,16 +2,19 @@ package main
 
 import (
 	"net/http"
+	"time"
 )
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
 	env := envelope{
-		"status" : "available",
-		"system_info" : map[string]string{
-			"environment" : app.config.env,
-			"version" : version,
+		"status": "available",
+		"system_info": map[string]string{
+			"environment": app.config.env,
+			"version":     version,
 		},
 	}
+
+	time.Sleep(4 * time.Second)
 
 	err := app.writeJSON(w, http.StatusOK, env, nil)
 	if err != nil {
